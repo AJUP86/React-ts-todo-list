b18e5be7f92996f4b821d620492e052ea9f1a7b3
Alejandro Urroz <alejandrourroz86@gmail.com>
2022-01-10 13:30:53 +0100

    Created style for drag and drop functionality

--- my-todo-ts/src/components/TodoList.tsx
+++ my-todo-ts/src/components/TodoList.tsx
@@ -6,10 +6,23 @@ import "./styles.css";
 const TodoList: React.FC = () => {
   const { todos } = useContext(TodosContext);
   return (
-    <div className="todos">
-      {todos.map((todo) => (
-        <SingleTodo todo={todo} key={todo.id} />
-      ))}
+    <div className="container">
+      <div className="todos">
+        <span className="todos__heading">
+          Active Tasks
+          {todos.map((todo) => (
+            <SingleTodo todo={todo} key={todo.id} />
+          ))}
+        </span>
+      </div>
+      <div className="todos remove">
+        <span className="todos__heading">
+          Completed Tasks
+          {todos.map((todo) => (
+            <SingleTodo todo={todo} key={todo.id} />
+          ))}
+        </span>
+      </div>
     </div>
   );
 };
